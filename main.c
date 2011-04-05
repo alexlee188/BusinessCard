3ea9591e9a48534be7732409f4400e9791d04227
alexlee188 <alexlee188@gmail.com>
2011-04-05 16:25:52 +0800

    flashyblinky

--- main.c
+++ main.c
@@ -67,8 +67,6 @@ void init(void)
 	
 	EEPROM_WP = 1;
 	EEPROM_WP_TRIS = 0;
-	_LATB7 = 0;
-	_TRISB7 = 0;
 	
 	PR2 = 0x03ff;
 	OC1R = 0x03FF;
@@ -83,40 +81,10 @@ void init(void)
 	AD1CON3bits.SAMC = 14;
 	_ADCS = 0x0; // change me if we switch clock freq
 	_ADON = 1;
+
+	_LATA2 = 0;
+    _LATA3 = 0;
 	
-	uart1_init(0x0C, 0x06, 0x05);
-	console_init(0x03);
-	
-	i2c2_init(0x15);
-	
-	if (eeprom_init(&eeprom_array_size))
-	{
-		// Bad things, we can't find even one EEPROM block. 
-		_LATB7 = 1;
-		while (1) {
-			delay_ticks(100);
-			_LATB7 = !_LATB7;
-		}
-	}
-	
-	eeprom_read_bytes(0, config, 4);
-	data_file = config[3];
-	eeprom_array_used = (((unsigned long) config[0]) << 16) + (config[1] << 8) + config[2];
-	eeprom_next_sample = eeprom_array_used / 3;
-	
-	if (data_file > 0x0E || eeprom_array_used + 3 >= eeprom_array_size - CONFIG_SIZE)
-	{
-		/* we are out of room, so we must enter UART I/O loop
-			and wait to be emptied */
-		_LATB7 = 1;
-		while (1) {
-			delay_ticks(1000);
-			_LATB7 = !_LATB7;
-		}
-	} else 
-	{
-		eeprom_write_byte(0x03, data_file + 1);
-	}
 }
 
 int main(void)
@@ -124,51 +92,13 @@ int main(void)
 	unsigned long long next_sample_at = 0;
 	unsigned int light = 0, temp = 0;
 	init();
-	
-	next_sample_at = ticks() + TICKS_PER_SAMPLE;
-	
-	while (1)
-	{
-		AD1CHS = 0x000B;
-		_SAMP = 1;
-		while (_SAMP || !AD1CON1bits.DONE) {}
-		light = ADC1BUF0;
-		OC1RS = 1023 - light;
-		if (ticks() >= next_sample_at)
-		{
-			AD1CHS = 0x000C;
-			_SAMP = 1;
-			while (_SAMP || !AD1CON1bits.DONE) {}
-			temp = ADC1BUF0;
 			
-			IEC1bits.T4IE = 0;
-			next_sample_at += TICKS_PER_SAMPLE;
-			eeprom_write_record(eeprom_next_sample, light, temp);
-			eeprom_next_sample++;
-			eeprom_array_used += 3;
-			if (eeprom_next_sample % 20 == 0)
-			{
-				eeprom_write_byte(0x00, eeprom_array_used >> 16);
-				eeprom_write_byte(0x01, eeprom_array_used >> 8);
-				eeprom_write_byte(0x02, eeprom_array_used);
-			}
-			
-			//if there is no more room, save our position and signal we need to be emptied
-			if (eeprom_array_used + 3 >= eeprom_array_size - CONFIG_SIZE)
-			{
-				eeprom_write_byte(0x00, eeprom_array_used >> 16);
-				eeprom_write_byte(0x01, eeprom_array_used >> 8);
-				eeprom_write_byte(0x02, eeprom_array_used);
-				IEC1bits.T4IE = 1;
-				
 				while (1) {
 					delay_ticks(1000);
-					_LATB7 = !_LATB7;
+					_LATA2 = !_LATA2;
+					_LATA3 = !_LATA3;
 				}
-			}
-			IEC1bits.T4IE = 1;
-		}
-	}
+
 	
 	return 0;
 }
